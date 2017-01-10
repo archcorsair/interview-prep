1521295e9571d5eea101dff2d60c460913875d5a
Daniel Shneyder <archcorsair@gmail.com>
2017-01-10 13:58:28 -0800

    ES6 shorthand return

--- src/main/java/com/cj/interview/prep/TimeStringApplication.js
+++ src/main/java/com/cj/interview/prep/TimeStringApplication.js
@@ -1,9 +1,9 @@
 timeString = '12:32 34:01 15:23 9:27 55:22 25:56'
 
 function timeStringApplication(times) {
-  const seconds = times.split(' ').map((time) => {
-    return time.split(':')
-  }).reduce((secs, entry) => {
+  const seconds = times.split(' ').map((time) =>
+  time.split(':')
+  ).reduce((secs, entry) => {
     return secs + parseInt(entry[0] * 60) + parseInt(entry[1])
   }, 0)
   const miliseconds = 1000 * seconds
@@ -17,4 +17,4 @@ function decimalToTime(date) {
 function assert(expected, error) {
   if (!expected) throw new Error(error)
 }
-console.log(assert(timeStringApplication(timeString) === '02:32:41', 'Time was not correct'))
+assert(timeStringApplication(timeString) === '02:32:41', 'Time was not correct')
